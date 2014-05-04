6e89cbdfa5d35bbff845596cfcf454d5e1a95bfe
YSUser <YSUser@users.noreply.github.com>
2014-05-04 09:56:35 +0300

    cleaned model

--- models/indexModel.php
+++ models/indexModel.php
@@ -6,11 +6,5 @@ class indexModel extends Model
 		parent::__construct();
 	}
 	
-	public function temp()
-	{
-		echo 'sdf';
-		$test = $this -> db -> query('SELECT * FROM customers');
-		var_dump($test -> fetch(PDO::FETCH_ASSOC));
-	}
 }
 ?>
